5941885348ec207256e188792584be0ee29baf00
Amanda Gonçalves <amandacgoncalves2002@gmail.com>
2022-04-20 12:59:49 -0300

    dataset

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -20,6 +20,8 @@ class UserController {
 
             let values = this.getValues();
 
+            if (!values) return false;
+
             this.getPhoto().then(
                 (content)=> {
 
@@ -90,7 +92,7 @@ class UserController {
 
         [...this.formEl.elements].forEach(function(field, index){
 
-            if (['name', 'email', 'password'].indexOf(field.name > -1 && !field.value)) {
+            if (['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value) {
 
                 field.parentElement.classList.add('has-error');
                 isValid = false;
@@ -140,6 +142,8 @@ class UserController {
 
         let tr = document.createElement('tr');
 
+        tr.dataset.user = JSON.stringify(dataUser);
+
         tr.innerHTML = `
             <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
             <td>${dataUser.name}</td>
@@ -153,7 +157,29 @@ class UserController {
         `;
 
         this.tableEl.appendChild (tr);
+
+        this.updateCount();
     
+    }
+
+    updateCount(){
+
+        let numberUsers = 0;
+        let numberAdmin = 0;
+        
+
+        [...this.tableEl.children].forEach(tr=>{
+
+            numberUsers++;
+
+                let user = JSON.parse(tr.dataset.user)
+
+                if (user._admin) numberAdmin++;
+        });
+
+        document.querySelector('#number-users').innerHTML = numberUsers;
+        document.querySelector('#number-users-admin').innerHTML = numberAdmin;
+
     }
 
 }
