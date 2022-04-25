0cb0c3983890d186759f2183fa5f1ecc58380751
Amanda Gonçalves <amandacgoncalves2002@gmail.com>
2022-04-25 12:37:31 -0300

    session storage

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -7,6 +7,7 @@ class UserController {
         this.tableEl = document.getElementById(tableId);
         this.onSubmit();
         this.onEditCancel();
+        this.selectAll();
     }
 
     onEditCancel(){
@@ -103,6 +104,8 @@ class UserController {
                 (content)=> {
 
                 values.photo = content;
+
+                this.insert(values);
 
                 this.addLine(values);
 
@@ -215,6 +218,48 @@ class UserController {
         
     }
 
+    getUsersStorage(){
+
+        let users = [];
+
+        if (sessionStorage.getItem('users')) {
+
+            users = JSON.parse(sessionStorage.getItem('users'));
+
+        }
+
+        return users;
+
+    }
+
+    selectAll(){
+
+
+        let users = this.getUsersStorage();
+
+        users.forEach(dataUser =>{
+
+            let user = new User();
+
+            user.loadFromJSON(dataUser);
+
+            this.addLine(user);
+
+        });
+
+
+    }
+
+    insert(data){
+
+        let users = this.getUsersStorage();
+
+        users.push(data);
+
+        sessionStorage.setItem('users', JSON.stringify(users));
+
+    }
+
     addLine(dataUser){
 
         let tr = document.createElement('tr');
@@ -229,7 +274,7 @@ class UserController {
             <td>${Utils.dateFormat(dataUser.register)}</td>
             <td>
                         <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-                        <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                        <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
                 </td>                     
         `;
 
@@ -242,6 +287,18 @@ class UserController {
     }
 
     addEventsTr(tr){
+
+        tr.querySelector('.btn-delete').addEventListener('click', e=>{
+
+            if (confirm('Deseja realmente excluir?')) {
+
+                tr.remove();
+
+                this.updateCount();
+
+            }
+
+        });
 
         tr.querySelector('.btn-edit').addEventListener('click', e=>{
 
